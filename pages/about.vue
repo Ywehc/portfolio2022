4838ba01c292b3614453d5e9bd2a98b58d4ea141
Chris <chris.kieran@gmail.com>
2021-04-30 20:42:11 -0400

    some page work

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,14 @@
   
 <template>
 <div class="container">
-    <div class="row">
+    <h1>About Me</h1>
+    <p>As a developer I'm drawn to the intersection of creativity and technical proficiency.</p>
+    <p>The career path I'm on is really exciting. I love bringing code to life on the frontend with Javascript (mostly Vue.js) and CSS, but have also built foundations in PHP and Python. These fundamentals are allowing me to get my feet wet with technologies like Deep Learning.</p>
+    <p>I enjoy nature, dogs, travel, music, astronomy, investing, fantasy sports, entrepreneurship and all sorts of other cool stuff. I'm fortunate to be able to spend lots of time with my amazing wife, son and dog, and they are a motivation boost every day.</p>
+    <div class="img-container">
+        <img src="~/assets/images/forest-extra-wide.png" alt="Chris and son walking in forest">
+    </div>
+    <!-- <div class="row">
         <div class="col">
             <h1>About Me</h1>
     <p>As a developer I'm drawn to the intersection of creativity and technical proficiency.</p>
@@ -12,7 +19,7 @@
                 <img src="~/assets/images/forest-extra-wide.png"
                     alt="Chris and son walking in forest">
         </div>
-    </div>
+    </div> -->
 </div>
     
 </template>
@@ -27,21 +34,17 @@ h1 {
 p {
     font-size: $paragraphFontSize;
 }
-.row {
-    height: 100%;
-}
-.img-container {
-    padding-left: 0;
-    position: relative;
-    margin-right: 10px;
-    img {
-        height: $paneHeight * 1.9;
-        position: absolute;
-        bottom: 0;
-        right: 0;
-    }
-}
 .container {
     padding: $containerPadding;
+    position: relative;
 }
+.img-container {
+    position: absolute;
+    bottom: 5px;
+    right: 15px;
+    img {
+        height: $paneHeight * 1.9;
+    }
+}
+
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,69 @@
 <template>
-<div class="container">
-<h2>CONTACT</h2>
-</div>
-    
+    <div class="container">
+        <h1 class="section-heading">Contact Me</h1>
+        <form   class="contact-form row" 
+                action="https://formspree.io/chriskierandevelopment@gmail.com" 
+                target="_blank" 
+                method="POST"
+        >   
+            <div class="col-12 col-sm-8 form-top">
+                <div class="form-group">
+                    <label for="name"></label>
+                    <input  class="form-input form-control" 
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            placeholder="Name" 
+                            required
+                    >
+                </div>
+                <div class="form-group">
+                    <label for="email"></label>
+                    <input  class="form-input form-control" 
+                            type="email" 
+                            name="email" 
+                            id="email" 
+                            placeholder="Email" 
+                            required
+                    >
+                </div>
+                <div class="form-group">
+                    <label for="phone"></label>
+                    <input  class="form-input form-control" 
+                            type="tel" 
+                            name="phone" 
+                            id="phone" 
+                            placeholder="Phone"
+                    >
+                </div> 
+            </div>
+            <div class="col-7 col-sm-4">
+                <a href="https://www.linkedin.com/in/chris-kieran" target="_blank">
+                    <div class="linkedin-container">
+                        <p class="linkedin">Connect with me on LinkedIn</p>
+                        <i class="fab fa-linkedin-in"></i>
+                    </div>
+                </a>              
+            </div> 
+                <div class="form-group col-12">
+                    <label for="message"></label>
+                    <textarea  
+                            class="form-input form-control" 
+                            rows="4"
+                            type="text" 
+                            name="message" 
+                            id="message"
+                            placeholder="How can I help you?" 
+                            required
+                    ></textarea>
+                    <div>
+                        <div>
+                            <button class="btn btn-outline-light button-submit" type="submit" value="Send">
+                                Send
+                            </button> 
+                        </div>
+                    </div>
+                </div>    
+        </form>
+    </div>
 </template>
