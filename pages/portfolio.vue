a010a87e74b90d5f31fb270127e9c871ee609f17
Chris <chris.kieran@gmail.com>
2021-05-16 00:12:16 -0400

    skills and portfolio looking better

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -2,36 +2,36 @@
     <div class="container">
         <div class="row">
             <div class="front col-6">
-                <h1>Portfolio</h1>
-                <div class="row">
-                    <div class="col-4">
-                        <div class="card">
-                            <img src="~/assets/images/cryptohawk.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Cryptohawk (2021)</h5>
-                                <p class="card-text">A cryptocurrency information and shopping app, built with the Vue.js ecosystem and Nomics API.</p>
-                            </div>
+                <a href="https://www.cryptohawk.ca" target="_blank">
+                    <div class="portfolio-card">
+                        <div class="card-heading">
+                            <h5 class="card-title">Cryptohawk (2021)</h5>
+                            <i class="fas fa-coins"></i>
                         </div>
+                        <p class="card-text">A cryptocurrency information and shopping app, built with the Vue.js ecosystem and Nomics API.</p>
                     </div>
-                    <div class="col-4">
-                        <div class="card">
-                            <img src="~/assets/images/propertrust.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Propertrust (2020)</h5>
-                                <p class="card-text">A Vue.js app that leans on Vuex state management to enable the user to generate a Sales agreement.</p>
-                            </div>
+                </a>
+                <a href="https://propertrust.chriskieran.com" target="_blank">
+                    <div class="portfolio-card">
+                        <div class="card-heading">
+                            <h5 class="card-title">Propertrust (2020)</h5>
+                            <i class="fas fa-file-signature"></i>
                         </div>
+                        <p class="card-text">A Vue.js app that uses state management and two-way data binding to enable the user to generate a Real Estate Sales agreement.</p>
                     </div>
-                    <div class="col-4">
-                        <div class="card">
-                            <img src="~/assets/images/hockeyquotes.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Hockey Quotes (2019)</h5>
-                                <p class="card-text">A fun React app built on Codepen which allows the user to generate a Hockey quote and Tweet it.</p>
-                            </div>
+                </a>
+                <a href="https://codepen.io/Ywehc/full/BMQEQW" target="_blank">
+                    <div class="portfolio-card">
+                        <div class="card-heading">
+                            <h5 class="card-title">Hockey Quotes (2019)</h5>
+                            <i class="fas fa-comment-dots"></i>
                         </div>
-                    </div>  
-                </div>
+                        <p class="card-text">A fun React app built on Codepen which allows the user to generate a famous NHL hockey quote and Tweet it.</p>
+                    </div>
+                </a>
+                <a class="btn btn-primary" href="https://github.com/Ywehc" role="button" target="_blank">
+                    <strong>Github  </strong><i class="fab fa-github"></i>
+                </a>
             </div>
             <div class="col-6 right">
                 <div class="title-container">
@@ -50,15 +50,9 @@ h1 {
     margin-bottom: $headingMarginBottom;
     font-size: $headingFontSize;
 }
-.card {
-    width: 18rem;
-    margin: auto;
-    img {
-        max-height: 220px;
-    }
-}
-.card-text {
-    font-size: 14px;
+a {
+    color: black;
+    text-decoration: none;
 }
 .container {
     padding: $containerPadding;
@@ -68,6 +62,8 @@ h1 {
 .front {
     z-index: $pageZIndex;
     position: relative; 
+    padding-left: 15em;
+    padding-top: 8em;
 }
 .img-portfolio {
     background-image: url('~/assets/images/portfolio.png');
@@ -83,8 +79,45 @@ h1 {
 .title-container {
     text-align: right;
     position: $imgContainerPosition;
-    bottom: -15px;
+    bottom: 40px;
     right: 10px;
     width: 100%;
+}
+.portfolio-card {
+    width: 450px;
+    border: 1px solid grey;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    padding: 10px;
+    h5 {
+        font-size: 20px;
+        margin-bottom: 4px;
+    }
+    p {
+        font-size: 14px;
+    }
+    i {
+    font-size: 20px;
+    }
+}
+.portfolio-card:hover {
+    border: 1px solid black;
+    background: black;
+    color: white;
+    cursor: pointer;
+}
+.card-heading {
+    display: flex;
+    justify-content: space-between;
+}
+.btn-primary {
+    color: black;
+    background: white;
+    border: 1px solid grey;
+}
+.btn-primary:hover {
+    background: black;
+    color: white;
+    border-color: black;
 }
 </style>
